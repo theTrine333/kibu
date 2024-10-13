b84423e045d615d4844c536d631529fc214edeed
theTrine333 <trinekaka@gmail.com>
2024-10-13 19:32:09 +0300

    Adding home page

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ loginBtn = document.getElementById("login-btn");
 let loginUsername = document.getElementById("login-username");
 let loginPassword = document.getElementById("login-password");
 
-loginBtn.addEventListener("click", e => {
-    e.preventDefault();
-    if (loginUsername.value == ""){
-        
-        document.getElementById("login-alert").className ="alert alert-danger alert-dismissible";
-        document.getElementById("login-alert").innerHTML = "Fill in your Username";
-    }else if(loginPassword.value == ""){
-        document.getElementById("login-alert").className ="alert alert-danger alert-dismissible";
-        document.getElementById("login-alert").innerHTML = "Fill in your Password";
-    }else{
-        // Perfom authentification from database
-        document.getElementById("login-span").className = "spinner-border spinner-border-sm";
-        document.getElementById("login-alert").className ="alert alert-success alert-dismissible";
-        document.getElementById("login-alert").innerHTML = "Authenticating...";
-    }
-    
-    
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (loginUsername.value == "") {
+    document.getElementById("login-alert").className =
+      "alert alert-danger alert-dismissible";
+    document.getElementById("login-alert").innerHTML = "Fill in your Username";
+  } else if (loginPassword.value == "") {
+    document.getElementById("login-alert").className =
+      "alert alert-danger alert-dismissible";
+    document.getElementById("login-alert").innerHTML = "Fill in your Password";
+  } else {
+    // Perfom authentification from database
+    document.getElementById("login-span").className =
+      "spinner-border spinner-border-sm";
+    document.getElementById("login-alert").className =
+      "alert alert-success alert-dismissible";
+    document.getElementById("login-alert").innerHTML = "Authenticating...";
+  }
 });
